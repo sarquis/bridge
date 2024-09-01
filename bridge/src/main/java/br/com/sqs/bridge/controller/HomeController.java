01f9200845aeca44a19a9a888a9c4d49fccf2b64
Roberto Sarquis <roberto.sarquis@gmail.com>
2024-08-31 21:56:19 -0300

    Colocando a versão da aplicação.

--- bridge/src/main/java/br/com/sqs/bridge/controller/HomeController.java
+++ bridge/src/main/java/br/com/sqs/bridge/controller/HomeController.java
@@ -1,13 +1,19 @@
 package br.com.sqs.bridge.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Value("${application.version}")
+    private String version;
+
     @GetMapping("/")
-    public String showHomePage() {
+    public String showHomePage(Model model) {
+	model.addAttribute("version", version);
 	return "homePage";
     }
 
