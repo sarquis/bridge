efd221644aeac1a349fcb98a0b2d1e4d7832c1f1
Roberto Sarquis <roberto.sarquis@gmail.com>
2024-09-03 21:19:19 -0300

    1.0.2

--- bridge/src/main/java/br/com/sqs/bridge/BridgeApplication.java
+++ bridge/src/main/java/br/com/sqs/bridge/BridgeApplication.java
@@ -2,29 +2,12 @@ package br.com.sqs.bridge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class BridgeApplication {
 
     public static void main(String[] args) {
-	System.out.println("TESTE");
-	// SpringApplication.run(BridgeApplication.class, args);
-	SpringApplication app = new SpringApplication(BridgeApplication.class);
-
-	ConfigurableApplicationContext context = app.run(args);
-	ConfigurableEnvironment env = context.getEnvironment();
-
-	String value1 = env.getProperty("spring.datasource.url");
-	String value2 = env.getProperty("spring.datasource.username");
-	String value3 = env.getProperty("spring.datasource.password");
-
-	System.out.println("Value 1: " + value1);
-	System.out.println("Value 2: " + value2);
-	System.out.println("Value 3: " + value3);
-
-	System.exit(0);
+	SpringApplication.run(BridgeApplication.class, args);
     }
 
 }
