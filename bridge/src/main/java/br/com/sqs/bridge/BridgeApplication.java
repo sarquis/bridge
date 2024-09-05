a92d5420922e5918197e3b50b65411c7e67e6f2c
Roberto Sarquis <roberto.sarquis@gmail.com>
2024-09-04 22:03:30 -0300

    1.0.4 Ajustando TimeZone.

--- bridge/src/main/java/br/com/sqs/bridge/BridgeApplication.java
+++ bridge/src/main/java/br/com/sqs/bridge/BridgeApplication.java
@@ -1,5 +1,7 @@
 package br.com.sqs.bridge;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BridgeApplication {
 
     public static void main(String[] args) {
+	TimeZone.setDefault(TimeZone.getTimeZone("America/Fortaleza"));
 	SpringApplication.run(BridgeApplication.class, args);
     }
 
